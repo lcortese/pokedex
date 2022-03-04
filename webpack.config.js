2534ca3a5dc86622ae9f574dd8a80fa8af6c07d5
Leandro Cortese <leandrocortese@gmail.com>
2022-03-04 17:02:46 -0300

    feat(build): configure webpack react and typescript

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
+module.exports = (env, argv) => {
+  const develop = argv.mode === 'development';
+
+  const devServer = develop ?
+    {
+      host: 'localhost',
+      port: 3000,
+      compress: true,
+      hot: true,
+    } : undefined;
+
+  const devtool = develop ? 'source-map' : undefined;
+
+  return {
+    entry: './src',
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+      rules: [{
+        test: /\.(ts|tsx)?$/,
+        include:  path.resolve(__dirname, 'src'),
+        loader: 'babel-loader',
+      }],
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js'],
+    },
+    devtool,
+    devServer,
+    target: 'web',
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'src', 'index.html'),
+      }),
+      devServer && new ForkTsCheckerWebpackPlugin()
+    ].filter(Boolean),
+  };
+};
